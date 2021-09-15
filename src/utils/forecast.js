3be176a78d8b150dee011f9e4f49d034f02a9bc5
Chirag Savsani <chirag.savsani@campusknot.com>
2021-09-15 14:53:16 +0530

    Humidity added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,15 @@ const forecast = (latitude, longitude, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=ca934cfa02a3bc42764bd2fb462b168d&query=' + latitude + ',' + longitude
 
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degree out. It is feels like " + body.current.feelslike + " degree out")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " 
+            + body.current.temperature + " degree out. It is feels like " 
+            + body.current.feelslike + " degree out. Humidity is " + body.current.humidity + "%.")
         }
     })
 }
